7fa3e8a498fe298f5af66e8ed46fd3e282b514f9
antonmaximus <anton23@gmail.com>
2015-10-15 16:50:35 -0700

    tweet

---
+++ config.js
@@ -0,0 +1,15 @@
+define({
+	ITEMS: [
+		{
+                  id: 'twitter-feed',
+                  type: 'user-twitter-feed',
+                  placeholderImage: './assets/twitter_300x250.png',
+                  width: 300,
+                  height: 250,
+
+                  // Enter Div Name where Twitter Feed with be placed
+                  twitterScreenName: '@nike',
+                  twitterWidgetId: ''
+		}
+    ]
+});
